94912a61dfe9040194e6c6e487cb56e7b233af70
Andrew Sauber <a3sauber@gmail.com>
2019-01-31 06:35:01 -0500

    template kubeadm config, deploy apiserver loadbalancer in advance

--- pkg/cloud/linode/lkesecrets.go
+++ pkg/cloud/linode/lkesecrets.go
@@ -20,10 +20,13 @@ import (
 	"bytes"
 	crand "crypto/rand"
 	"encoding/hex"
-	"github.com/golang/glog"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"text/template"
+
+	"github.com/golang/glog"
 
 	"golang.org/x/net/context"
 	corev1 "k8s.io/api/core/v1"
@@ -50,7 +53,6 @@ type certsInit = struct {
 }
 
 func run(prog string, args ...string) (string, error) {
-
 	var stdout, stderr bytes.Buffer
 	cmd := exec.Command(prog, args...)
 	cmd.Stdout = &stdout
@@ -60,38 +62,97 @@ func run(prog string, args ...string) (string, error) {
 	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
 	if outStr != "" {
 		// we can print stdout here
+		glog.Infof("stdout:\n%v", outStr)
 	}
 	if errStr != "" {
-		// we can print stderr here
+		glog.Infof("stderr:\n%v", errStr)
 	}
 
 	return outStr, err
 }
 
-func getKubeadmConfig(client client.Client, cluster *clusterv1.Cluster) ([]byte, error) {
+type kubeadmConfigParams struct {
+	NodeBalancerHostname string
+	ClusterName          string
+	CertsDir             string
+}
 
-	// I am the vine, ye are the branches: He that abideth in me, and I in him,
-	// the same bringeth forth much fruit: for without me ye can do nothing
-	return nil, nil
+const kubeadmConfigTemplate = `kind: ClusterConfiguration
+apiVersion: kubeadm.k8s.io/v1beta1
+apiServer:
+  certSANs:
+	- {{ .NodeBalancerHostname }}
+	- kube-apiserver.kube-system-{{ .ClusterName }}.svc.cluster.local
+	- localhost
+  extraArgs:
+    authorization-mode: Node,RBAC
+    cloud-provider: external
+    feature-gates: CSINodeInfo=true,CSIDriverRegistry=true,BlockVolume=true,CSIBlockVolume=true
+  timeoutForControlPlane: 4m0s
+certificatesDir: {{ .CertsDir }}
+clusterName: {{ .ClusterName }}
+controlPlaneEndpoint: ""
+controllerManager:
+  extraArgs:
+    cloud-provider: external
+    feature-gates: ""
+dns:
+  type: CoreDNS
+etcd:
+  local:
+    dataDir: /var/lib/etcd
+    serverCertSANs:
+      - etcd.kube-system.svc.cluster.local
+    peerCertSANs:
+      - etcd.kube-system.svc.cluster.local
+imageRepository: k8s.gcr.io
+kubernetesVersion: v1.13.2
+networking:
+  dnsDomain: cluster.local
+  podSubnet: 10.2.0.0/16
+  serviceSubnet: 10.128.0.0/16
+scheduler: {}
+`
 
+func getKubeadmConfig(client client.Client, cluster *clusterv1.Cluster, dirname string) ([]byte, error) {
+	if len(cluster.Status.APIEndpoints) < 1 {
+		return nil, fmt.Errorf("No APIEndpoints while writing certs for cluster (LoadBalancer Service not provisioned?) %v", cluster.Name)
+	}
+
+	configParams := kubeadmConfigParams{
+		NodeBalancerHostname: cluster.Status.APIEndpoints[0].Host,
+		ClusterName:          cluster.Name,
+		CertsDir:             dirname,
+	}
+
+	tmpl, err := template.New("kubeadm-config").Parse(kubeadmConfigTemplate)
+	if err != nil {
+		return nil, err
+	}
+
+	var configBuf bytes.Buffer
+	if err := tmpl.Execute(&configBuf, configParams); err != nil {
+		return nil, err
+	}
+
+	return configBuf.Bytes(), nil
 }
 
 func createKubeadmFile(client client.Client, dirname string, cluster *clusterv1.Cluster) (string, error) {
-
 	filename := dirname + "/" + "kubeadm.conf"
 
-	if data, err := getKubeadmConfig(client, cluster); err != nil {
+	fmt.Printf(filename)
+
+	if data, err := getKubeadmConfig(client, cluster, dirname); err != nil {
 		return "", err
 	} else if err := ioutil.WriteFile(filename, data, 0644); err != nil {
 		return "", err
 	}
 
 	return filename, nil
-
 }
 
 func generateCertsInit(client client.Client, cluster *clusterv1.Cluster) (*certsInit, error) {
-
 	dirname := "/tmp/" + cluster.Name + "/pki"
 	if err := os.MkdirAll(dirname, os.ModePerm); err != nil {
 		return nil, err
@@ -104,17 +165,14 @@ func generateCertsInit(client client.Client, cluster *clusterv1.Cluster) (*certs
 	}
 
 	return &certsInit{dir: dirname}, nil
-
 }
 
 func generateCertsFini(init *certsInit) {
-
 	if dir, err := ioutil.ReadDir(init.dir); err == nil {
 		for _, d := range dir {
 			os.RemoveAll(init.dir + "/" + d.Name())
 		}
 	}
-
 }
 
 /*
@@ -131,18 +189,15 @@ func generateCertsFini(init *certsInit) {
  *     certs["ca.crt"] = $(cat /etc/lke0/pki/etcd/ca.crt)
  */
 func addFile(init *certsInit, certs map[string][]byte, file, subpath string) error {
-
 	data, err := ioutil.ReadFile(init.dir + "/" + subpath + "/" + file)
 	if err != nil {
 		return err
 	}
 	certs[file] = data
 	return nil
-
 }
 
 func addFiles(init *certsInit, certs map[string][]byte, keyval map[string]string) error {
-
 	for key, value := range keyval {
 		err := addFile(init, certs, key, value)
 		if err != nil {
@@ -150,11 +205,9 @@ func addFiles(init *certsInit, certs map[string][]byte, keyval map[string]string
 		}
 	}
 	return nil
-
 }
 
 func generateCerts(client client.Client, cluster *clusterv1.Cluster) (map[string][]byte, map[string][]byte, error) {
-
 	init, err := generateCertsInit(client, cluster)
 	if err != nil {
 		return nil, nil, err
@@ -201,7 +254,6 @@ func generateCerts(client client.Client, cluster *clusterv1.Cluster) (map[string
 }
 
 func generateCertSecrets(client client.Client, cluster *clusterv1.Cluster) error {
-
 	k8sCerts, etcdCerts, err := generateCerts(client, cluster)
 	if err != nil {
 		return err
@@ -225,7 +277,6 @@ func generateCertSecrets(client client.Client, cluster *clusterv1.Cluster) error
  * <token> is a crypto-safe random (printable) string
  */
 func generateNodeWatcherToken() ([]byte, error) {
-
 	binToken := make([]byte, 32)
 	if _, err := crand.Read(binToken); err != nil {
 		return nil, err
@@ -262,14 +313,15 @@ func generateNodeWatcherSecrets(client client.Client, cluster *clusterv1.Cluster
  * create secrets needed for operation of control plane components
  */
 func (lcc *LinodeClusterClient) generateSecrets(cluster *clusterv1.Cluster) error {
-
 	glog.Infof("Creating secrets for cluster %v.", cluster.Name)
 
 	if err := generateCertSecrets(lcc.client, cluster); err != nil {
+		glog.Errorf("Error generating certs for cluster %v: %v.", cluster.Name, err)
 		return err
 	}
 
 	if err := generateNodeWatcherSecrets(lcc.client, cluster); err != nil {
+		glog.Errorf("Error generating NodeWatcher token for cluster %v: %v.", cluster.Name, err)
 		return err
 	}
 
