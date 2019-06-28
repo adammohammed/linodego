0bb0ab073182d22f446c4386eb629615505dd265
Andrew Sauber <a3sauber@gmail.com>
2019-06-27 20:31:52 -0400

    update revision.sh

--- revision.sh
+++ revision.sh
@@ -1,3 +1,3 @@
 VERSION=$(git rev-parse HEAD | head -c 12)
-DIRTY=$(git diff-index --quiet HEAD -- || echo "dirty")
-echo "$VERSION-$DIRTY"
+DIRTY=$(git diff-index --quiet HEAD -- || echo "-dirty")
+echo "$VERSION$DIRTY"
