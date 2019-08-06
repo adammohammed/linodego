596512fb29ad9fcf08230c178606ef1ef0c14acc
John Schaeffer <jschaeffer@linode.com>
2019-08-06 11:32:58 -0400

    Updated template generation script to have command-line arguments.

--- examples/generate-yaml.sh
+++ examples/generate-yaml.sh
@@ -5,34 +5,73 @@ set -o pipefail
 
 die() { echo "$*" 1>&2 ; exit 1; }
 
-[ "$#" -eq 5 ] || die 'First argument must be a path to an ssh public key (for accessing Nodes of the cluster).
-Second argument must be a name for the cluster.
-Third argument must be an S3 style AWS_ACCESS_KEY_ID
-Fourth argument must be an S3 style AWS_SECRET_ACCESS_KEY
-Fifth argument must be an S3-style AWS endpoint, without bucket
-Sixth argument must be an S3-style bucket
-Seventh argument must be a machine name
-Eighth argument must be a pool ID
+USAGE="Usage: generate-yaml.sh -t linode-token -r linode-region -k pubkey -c cluster-name -a obj-access-key -s obj-secret-key -e obj-endpoint -b obj-bucket -m machine-name -p pool-id"
 
-For example:
-./generate-yaml.sh $HOME/.ssh/id_rsa.pub cluster01 $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $AWS_ENDPOINT $AWS_BUCKET $MACHINE_NAME $POOL_ID'
+while (( "$#" )); do
+    case "$1" in
+        -t|--linode-token)
+            LINODE_TOKEN=$2
+            shift 2
+            ;;
+        -r|--linode-region)
+            LINODE_REGION=$2
+            shift 2
+            ;;
+	-k|--public-key)
+            PUBLIC_KEY_PATH=$2
+            shift 2
+            ;;
+        -c|--cluster-name)
+            CLUSTER_NAME=$2
+            shift 2
+            ;;
+        -a|--obj-access-key)
+            OBJ_ACCESS_KEY=$2
+            shift 2
+            ;;
+        -s|--obj-secret-key)
+            OBJ_SECRET_KEY=$2
+            shift 2
+            ;;
+        -e|--obj-endpoint)
+            OBJ_ENDPOINT=$2
+            shift 2
+            ;;
+        -b|--obj-bucket)
+            OBJ_BUCKET=$2
+            shift 2
+            ;;
+        -m|--machine-name)
+            MACHINE_NAME=$2
+            shift 2
+            ;;
+        -p|--pool-id)
+            POOL_ID=$2
+            shift 2
+            ;;
+        -h|--help)
+            die "$USAGE"
+            ;;
+        *)
+            echo "unknown argument $1" 1>&2
+            die "$USAGE"
+            ;;
+    esac
+done
 
-[ -z "${LINODE_TOKEN}" ] && die "\$LINODE_TOKEN must be set to a Linode API token"
-[ -z "${LINODE_REGION}" ] && die "\$LINODE_REGION must be set to a Linode Region ID"
+[ -z "${LINODE_TOKEN}" ] && die "LINODE_TOKEN must be set to a Linode API token"
+[ -z "${LINODE_REGION}" ] && die "LINODE_REGION must be set to a Linode Region ID"
 
-PUBLIC_KEY=$(cat $1)
-CLUSTER_NAME=$2
+PUBLIC_KEY=$(cat $PUBLIC_KEY_PATH)
 ENCODED_TOKEN=$(echo -n $LINODE_TOKEN | base64)
 ENCODED_REGION=$(echo -n $LINODE_REGION | base64)
-ENCODED_ACCESS_KEY=$(echo -n $3 | base64)
-ENCODED_SECRET_KEY=$(echo -n $4 | base64)
-ENCODED_AWS_ENDPOINT=$(echo -n $5 | base64)
-ENCODED_AWS_BUCKET=$(echo -n $6 | base64)
-MACHINE_NAME="$7"
-POOL_ID="$8"
+ENCODED_ACCESS_KEY=$(echo -n $OBJ_ACCESS_KEY | base64)
+ENCODED_SECRET_KEY=$(echo -n $OBJ_SECRET_KEY | base64)
+ENCODED_AWS_ENDPOINT=$(echo -n $OBJ_ENDPOINT | base64)
+ENCODED_AWS_BUCKET=$(echo -n $OBJ_BUCKET | base64)
 
 cat cluster.yaml.template |
-sed -e "s|\$SSH_PUBLIC_KEY|$(cat $1)|" |
+sed -e "s|\$SSH_PUBLIC_KEY|$PUBLIC_KEY|" |
 sed -e "s|\$LINODE_TOKEN|$ENCODED_TOKEN|" |
 sed -e "s|\$LINODE_REGION|$ENCODED_REGION|" |
 sed -e "s|\$AWS_ACCESS_KEY_ID|$ENCODED_ACCESS_KEY|" |
