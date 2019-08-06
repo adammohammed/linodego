ce2ed83f8900cbbc0447ae1ce67a7405f627f364
John Schaeffer <jschaeffer@linode.com>
2019-08-06 09:48:21 -0400

    Added AWS_BUCKET parameter to template generation

--- examples/generate-yaml.sh
+++ examples/generate-yaml.sh
@@ -10,11 +10,12 @@ Second argument must be a name for the cluster.
 Third argument must be an S3 style AWS_ACCESS_KEY_ID
 Fourth argument must be an S3 style AWS_SECRET_ACCESS_KEY
 Fifth argument must be an S3-style AWS endpoint, without bucket
-Sixth argument must be a machine name
-Seventh argument must be a pool ID
+Sixth argument must be an S3-style bucket
+Seventh argument must be a machine name
+Eighth argument must be a pool ID
 
 For example:
-./generate-yaml.sh $HOME/.ssh/id_rsa.pub cluster01 $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $AWS_ENDPOINT $MACHINE_NAME $POOL_ID'
+./generate-yaml.sh $HOME/.ssh/id_rsa.pub cluster01 $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $AWS_ENDPOINT $AWS_BUCKET $MACHINE_NAME $POOL_ID'
 
 [ -z "${LINODE_TOKEN}" ] && die "\$LINODE_TOKEN must be set to a Linode API token"
 [ -z "${LINODE_REGION}" ] && die "\$LINODE_REGION must be set to a Linode Region ID"
@@ -26,8 +27,9 @@ ENCODED_REGION=$(echo -n $LINODE_REGION | base64)
 ENCODED_ACCESS_KEY=$(echo -n $3 | base64)
 ENCODED_SECRET_KEY=$(echo -n $4 | base64)
 ENCODED_AWS_ENDPOINT=$(echo -n $5 | base64)
-MACHINE_NAME="$6"
-POOL_ID="$7"
+ENCODED_AWS_BUCKET=$(echo -n $6 | base64)
+MACHINE_NAME="$7"
+POOL_ID="$8"
 
 cat cluster.yaml.template |
 sed -e "s|\$SSH_PUBLIC_KEY|$(cat $1)|" |
@@ -35,6 +37,7 @@ sed -e "s|\$LINODE_TOKEN|$ENCODED_TOKEN|" |
 sed -e "s|\$LINODE_REGION|$ENCODED_REGION|" |
 sed -e "s|\$AWS_ACCESS_KEY_ID|$ENCODED_ACCESS_KEY|" |
 sed -e "s|\$AWS_SECRET_ACCESS_KEY|$ENCODED_SECRET_KEY|" |
+sed -e "s|\$AWS_BUCKET|$ENCODED_AWS_BUCKET|" |
 sed -e "s|\$AWS_ENDPOINT|$ENCODED_AWS_ENDPOINT|" |
 sed -e "s|\$CLUSTER_NAME|$CLUSTER_NAME|" > cluster.yaml
 
